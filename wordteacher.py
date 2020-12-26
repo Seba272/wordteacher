59dec5a1a0688b262e42115a3b03857602c4cdd6
Seba272 <git@sebang.fastmail.fm>
2020-12-26 13:14:45 +0100

    Change name of file and a couple of other stuff.

--- wordteacher.py
+++ wordteacher.py
@@ -4,12 +4,13 @@ import random
 import json
 from os import path
 
-this_file = "word_teacher_v0.2"
+this_file = "word_teacher_v0.3"
 log_file = "wt.log"
 log_separator = " ; "
-diz_data_file = "diz_data_file2"
-diz_file="dict"
+diz_file="Suomi-English.csv"
+diz_data_file = diz_file + ".wt"
 diz_data=[] 
+strat_exercise = [2,2,2,2,2]
 
 def write_log(qst,ans_c,ans):
     with open(log_file,"a") as fl:
@@ -24,25 +25,55 @@ def write_log(qst,ans_c,ans):
         fl.write(ans)
         fl.write(" \n")
 
-# [[0:lang0, 1:lang1, 2:number of positive tests, 3:number of negative tests, 4:date of last test],...]
-
+# [[0:lang0, 1:lang1, 2:number of positive tests, 3:number of negative tests, 4:date of inizialization, 5:date of last test],...]
 def diz_upload(f=diz_file):
     dd = []
+    today = int(time.time())
     with open(f,"r") as fd:
         for line in fd:
             if line[0]=="%":
                 continue
             if line[0]=="!":
-                #line = line[1:].strip().replace(" ","")
                 line = line[1:]
-                #line = lang.split(";")
             a = line.split(";")
             a = [b.strip() for b in a]
-            a.append(0)
-            a.append(0)
-            a.append(0)
+            a.append(0) # 2: positive tests
+            a.append(0) # 3: negative tests
+            a.append(today) # 4: date of inizialization
+            a.append(0) # 5: date of last test
             dd.append(a)
     return dd
+
+def diz_data_save(f=diz_data_file,dd=diz_data):
+    with open(f,"w") as fl:
+        json.dump(dd,fl)
+
+def diz_data_upload(f=diz_data_file,dd=diz_data):
+    if path.exists(diz_data_file):
+        with open(diz_data_file, "r") as fup:
+            dd = json.load(fup)
+    else:
+        dd = diz_upload()
+    return dd
+
+# [[index in diz,level],...]
+def choose_batch(dd=diz_data,n_words=5):
+    a = []
+    today = int(time.time())
+    strat_repetitions = max(strat_exercise)
+    strat_days = len(strat_exercise)
+#    print("choose_batch: diz_data, dd: \n",dd)
+#    print("choose_batch: today \n",today)
+    for w in range(1,len(dd)) :
+        word_days = max(int((dd[w][5]-today)/86400),0)
+        word_level = dd[w][2]-dd[w][3]
+#        print("choose_batch: w=",w,"; word_days=",word_days,"; word_level=",word_level,"; strat_days=",strat_days,"; sum=",sum([strat_exercise[j] for j in range(word_days)]),"; k=",k,"\n")
+        if word_days < strat_days and word_level < sum([strat_exercise[j] for j in range(word_days+1)]) :
+            a.append([w,strat_exercise[word_days]])
+        if len(a) >= n_words :
+            break
+#    print("choose_batch: a:\n",a)
+    return a
 
 def testing(test_batch):
     number_words = len(test_batch)
@@ -65,40 +96,6 @@ def testing(test_batch):
                 test_batch[k][1] += 1
                 print("No, the right answer is: " + rightans)
             diz_data[test_batch[k][0]][4]=time.time().__int__()
-
-def diz_data_save(f=diz_data_file,dd=diz_data):
-    with open(f,"w") as fl:
-        json.dump(dd,fl)
-
-def diz_data_upload(f=diz_data_file,dd=diz_data):
-    if path.exists(diz_data_file):
-        with open(diz_data_file, "r") as fup:
-            dd = json.load(fup)
-    else:
-        dd = diz_upload()
-    return dd
-
-# [[index in diz,level],...]
-def choose_batch(dd=diz_data,n_words=5):
-    a = []
-    k = 1
-    today = int(time.time())
-    strat_exercise = [5,5,3,2,1]
-    strat_repetitions = max(strat_exercise)
-    strat_days = len(strat_exercise)
-#    print("choose_batch: diz_data, dd: \n",dd)
-#    print("choose_batch: today \n",today)
-    for w in range(1,len(dd)) :
-        k+=1
-        word_days = max(int((dd[w][4]-today)/86400),0)
-        word_level = dd[w][2]-dd[w][3]
-#        print("choose_batch: w=",w,"; word_days=",word_days,"; word_level=",word_level,"; strat_days=",strat_days,"; sum=",sum([strat_exercise[j] for j in range(word_days)]),"; k=",k,"\n")
-        if word_days < strat_days and word_level < sum([strat_exercise[j] for j in range(word_days+1)]) :
-            a.append([w,strat_exercise[word_days]])
-        if k >= n_words :
-            break
-#    print("choose_batch: a:\n",a)
-    return a
 
 
 diz_data = diz_data_upload()
