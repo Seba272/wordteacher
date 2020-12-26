a7eb3ac26c4ea325d4eb7d5af347e310226a41a8
Seba272 <git@sebang.fastmail.fm>
2020-12-26 17:06:31 +0100

    Add a menu and a function to repeat known words.

--- wordteacher.py
+++ wordteacher.py
@@ -2,19 +2,17 @@
 import time
 import random
 import json
-from os import path
+import os
+#from os import path
 import sys 
 from tabulate import tabulate
 
-this_file = "word_teacher_v1.0"
+this_file = "word_teacher_v1.1"
 log_file = "wt.log"
 log_separator = " ; "
 
 class dizionario:
-#    self.theme = theme
     strategy = [2,2,2,2,2]
-    #theme = f_origin.split(".")[0]
-    #f_data_name = "." + theme + ".wt"
     
     def __init__(self,theme):
         #nonlocal self.data, self.theme
@@ -23,10 +21,11 @@ class dizionario:
         self.f_origin_name = self.theme + ".csv"
         self.f_data_name = "." + self.theme + ".wt"
         self.data = []
-        if path.exists(self.f_data_name):
+# [[0:lang0, 1:lang1, 2:number of positive tests, 3:number of negative tests, 4:date of inizialization, 5:date of last test],...]
+        if os.path.exists(self.f_data_name):
             with open(self.f_data_name, "r") as f_data:
                 self.data = json.load(f_data)
-        elif path.exists(self.f_origin_name):
+        elif os.path.exists(self.f_origin_name):
             with open(self.f_origin_name,"r") as f_origin:
                 today = int(time.time())
                 for line in f_origin:
@@ -49,8 +48,8 @@ class dizionario:
         with open(self.f_data_name,"w") as f_data:
             json.dump(self.data,f_data)
     
-    # [[index in diz,level],...]
-    def make_batch(self,n_words=5):
+    # [[index in diz, how many repetitions],...]
+    def make_batch_learn(self,n_words=5):
         batch = []
         today = int(time.time())
         strat_repetitions = max(self.strategy)
@@ -65,13 +64,27 @@ class dizionario:
     #    print("choose_batch: a:\n",a)
         return batch
 
+    # [[index in diz, how many repetitions],...]
+    def make_batch_repeat(self,n_words=5):
+        batch = []
+        words_active = []
+        for w in range(len(self.data)) :
+            word_touched = self.data[w][2] + self.data[w][3]
+            word_level = self.data[w][2] - self.data[w][3]
+            if word_touched != 0 :
+                words_active.append([w,word_level])
+        words_active.sort(key = lambda k: k[1])
+        batch = [[words_active[k][0],2] for k in range(n_words)]
+        return batch
+
     def print_status(self):
         print("Name of dictionary: ",self.theme)
         print("File of origin: ",self.f_origin_name)
         print("Database: ",self.f_data_name)
         print("Number of words: ",len(self.data))
-        yn = input("Do you want a printout of the dictionary? ")
+        yn = input("Do you want a printout of the whole dictionary? ")
         if yn == "y" or yn == "yes" :
+            f_status_name = input("Where?")
             header = [self.languages[0],self.languages[1],"Tests","Level","When uploaded","Last tested"]
             table = []
             for word in self.data :
@@ -83,58 +96,8 @@ class dizionario:
                         time.strftime("%Y/%m/%d %H:%M:%S",time.localtime(word[4])),\
                         time.strftime("%Y/%m/%d %H:%M:%S",time.localtime(word[5]))\
                         ])
-            print(tabulate(table,header))
-        
-    
-#    def diz_upload(f=diz_file):
-#        dd = []
-#        today = int(time.time())
-#        with open(f,"r") as fd:
-#            for line in fd:
-#                if line[0]=="%":
-#                    continue
-#                if line[0]=="!":
-#                    line = line[1:]
-#                a = line.split(";")
-#                a = [b.strip() for b in a]
-#                a.append(0) # 2: positive tests
-#                a.append(0) # 3: negative tests
-#                a.append(today) # 4: date of inizialization
-#                a.append(0) # 5: date of last test
-#                dd.append(a)
-#        return dd
-    
-#    def diz_data_save(f=diz_data_file,dd=diz_data):
-#        with open(f,"w") as fl:
-#            json.dump(dd,fl)
-    
-#    def diz_data_upload(f=diz_data_file,dd=diz_data):
-#        if path.exists(diz_data_file):
-#            with open(diz_data_file, "r") as fup:
-#                dd = json.load(fup)
-#        else:
-#            dd = diz_upload()
-#        return dd
-    
-    # [[index in diz,level],...]
-#    def choose_batch(dd=diz_data,n_words=5):
-#        a = []
-#        today = int(time.time())
-#        strat_repetitions = max(strat_exercise)
-#        strat_days = len(strat_exercise)
-#    #    print("choose_batch: diz_data, dd: \n",dd)
-#    #    print("choose_batch: today \n",today)
-#        for w in range(1,len(dd)) :
-#            word_days = max(int((dd[w][5]-today)/86400),0)
-#            word_level = dd[w][2]-dd[w][3]
-#    #        print("choose_batch: w=",w,"; word_days=",word_days,"; word_level=",word_level,"; strat_days=",strat_days,"; sum=",sum([strat_exercise[j] for j in range(word_days)]),"; k=",k,"\n")
-#            if word_days < strat_days and word_level < sum([strat_exercise[j] for j in range(word_days+1)]) :
-#                a.append([w,strat_exercise[word_days]])
-#            if len(a) >= n_words :
-#                break
-#    #    print("choose_batch: a:\n",a)
-#        return a
-    
+            with open(f_status_name,"w") as f_status :
+                f_status.write(tabulate(table,header))
 
 def write_log(qst,ans_c,ans):
     with open(log_file,"a") as fl:
@@ -149,7 +112,6 @@ def write_log(qst,ans_c,ans):
         fl.write(ans)
         fl.write(" \n")
 
-# [[0:lang0, 1:lang1, 2:number of positive tests, 3:number of negative tests, 4:date of inizialization, 5:date of last test],...]
 def testing(test_batch,from_diz):
     number_words = len(test_batch)
     while number_words > 0 :
@@ -169,17 +131,71 @@ def testing(test_batch,from_diz):
             else :
                 from_diz.data[test_batch[k][0]][3] += 1
                 test_batch[k][1] += 1
-                print("No, the right answer is: " + rightans)
-            from_diz.data[test_batch[k][0]][5]=time.time().__int__()
+                print("No, the right answer is: ", rightans)
+            from_diz.data[test_batch[k][0]][5] = int(time.time())
 
-theme = input("Which theme? ")
-length_batches = int(input("How many words? "))
-diz = dizionario(theme)
-#lang = diz.languages
-test_batch = diz.make_batch(length_batches)
-#testing(test_batch,diz)
-diz.save_data()
-diz.print_status()
+def learn():
+    length_batch = int(input("How many words? "))
+    test_batch = diz.make_batch_learn(length_batch)
+    testing(test_batch,diz)
+    diz.save_data()
+
+def repeat():
+    length_batch = int(input("How many words? "))
+    test_batch = diz.make_batch_repeat(length_batch)
+    testing(test_batch,diz)
+    diz.save_data()
+
+# Menu
+while 1:
+    print("What do you want to do?")
+    print("0. Upload a new dictionary")
+    files = os.listdir(".")
+    dictionaries = []
+    for f in files:
+        a = f.split(".")[-1]
+        if a == "wt" :
+            dictionaries.append(f)
+    k = 1
+    for f in dictionaries:
+        ff = f.split(".")[-2]
+        print(str(k)+". Use ",ff)
+        k+=1
+    print("q. Exit")
+    ans = input()
+    if ans == "q" :
+        sys.exit()
+    ans = int(ans)
+    if ans == 0:
+        theme = input("Which theme?")
+    elif ans <= len(dictionaries) :
+        theme = dictionaries[ans-1].split(".")[-2]
+    else :
+        print("There is not such option.")
+        continue
+    diz = dizionario(theme)
+    while 1:
+        print("What do you want to do?")
+        print("1. Go on with learning")
+        print("2. Repeat learned words")
+        print("3. Print out status of dictionary")
+        print("q. Return to main menu")
+        ans = input()
+        if ans == "q":
+            break
+        elif ans == "1":
+            learn()
+        elif ans == "2":
+            repeat()
+        elif ans == "3":
+            diz.print_status()
+        else:
+            print("There is not such option.")
+            continue
+
+
+
+
 
 
 
