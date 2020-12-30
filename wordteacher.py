75c2bb998a3e9f5cd6b0ddb2ee651d24427a8eee
Seba272 <git@sebang.fastmail.fm>
2020-12-30 09:22:16 +0100

    Update version number in file.

--- wordteacher.py
+++ wordteacher.py
@@ -7,7 +7,7 @@ import os
 import sys 
 from tabulate import tabulate
 
-version = "v2.2-dev"
+version = "v2.3-dev"
 this_file = "word_teacher_"+version
 #path_for_wt = "/Users/lupo/.wordteacher/"
 path_for_wt = "/Users/lupo/.wordteacher/dev-datafolder/"
